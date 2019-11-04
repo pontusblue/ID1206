136a15b3dcc48a518ed82c00fc3634310aff171d
Pontus Asp <pontus.asp@gmail.com>
2019-11-04 15:42:08 +0100

    a2_2.1

--- assignments/a2/code.c
+++ assignments/a2/code.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 int main()
 {
@@ -8,12 +10,15 @@ int main()
     if(pid == 0)
     {
         printf("I'm the child %d\n", getpid());
+        sleep(1);
     }
     else
     {
         printf("My child is called %d\n", pid);
+        wait(NULL);
+        printf("My child %d has been terminated :( says %d\n", pid, getpid());
     }
-    printf("That's it %d\n", getpid());
+    printf("This is the end, says %d\n", getpid());
 
     return 0;
 }
