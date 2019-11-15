a123a5728a65ba03efbcaf7247282dd33a18359c
Pontus Asp <pontus.asp@gmail.com>
2019-11-15 10:56:08 +0100

    s1_p1

---
+++ sem/s1/myos.c
@@ -0,0 +1,26 @@
+#define COLUMNS 80
+#define ROWS 25
+
+char *name = "My OS is running";
+
+typedef struct vga_char {
+	char character;
+	char colors;
+} vga_char;
+
+void main(void) {
+
+	// The VGA terminal mapped memory
+	vga_char *vga = (vga_char*)0xb8000;
+
+	for(int i = 0; i < COLUMNS*ROWS; i++) {
+		vga[i].character = ' ';
+		vga[i].colors = 0x0c;
+	}
+
+	for(int i = 0; name[i] != '\0'; i++) {
+		vga[600+i].character = name[i];
+		vga[600+i].colors = 0x0c;
+	}
+	return;
+}
