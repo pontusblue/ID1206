55e62f0b6a0e0b9ac55caa35c4c30fb4b96437c5
Pontus Asp <pontus.asp@gmail.com>
2019-11-15 10:51:37 +0100

    s1_print-pontus

--- sem/s1/kernel.asm
+++ sem/s1/kernel.asm
@@ -4,5 +4,10 @@ global start
 section .text
 
 start:
-	mov dword [0xb8000], 0x2f4b2f4f		; print 'OK' to screen
+	mov word [0xb8000], 0x8b50		; print P
+	mov word [0xb8002], 0x8b6f		; print o
+	mov word [0xb8004], 0x8b6e		; print n
+	mov word [0xb8006], 0x8b74		; print t
+	mov word [0xb8008], 0x8b75		; print u
+	mov word [0xb800a], 0x8f73		; print s
 	hlt
