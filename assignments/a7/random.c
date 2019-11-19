6d9705245fba54a80e5cf394628fc9d46eb21274
Pontus Asp <pontus.asp@gmail.com>
2019-11-19 13:54:47 +0100

    a7_1.3

--- assignments/a7/random.c
+++ assignments/a7/random.c
@@ -5,6 +5,7 @@
 
 #define HIGH 20
 #define FREQ 80
+#define SAMPLES 20
 
 typedef struct pte {
 	int present;
@@ -24,6 +25,45 @@ void init(int *sequence, int refs, int pages)
 	}
 }
 
+void clear_page_table(pte *page_table, int pages)
+{
+	for(int i = 0; i < pages; i++) {
+		page_table[i].present = 0;
+	}
+}
+
+int simulate(int *seq, pte *table, int refs, int frms, int pgs)
+{
+	int hits = 0;
+	int allocated = 0;
+
+	int i;
+
+	for(i = 0; i < refs; i++) {
+		int next = seq[i];
+		pte *entry = &table[next];
+
+		if(entry->present == 1) {
+			hits++;
+		} else {
+			if(allocated < frms) {
+				allocated++;
+				entry->present = 1;
+			} else {
+				pte *evict;
+				do {
+					int rnd = rand() % pgs;
+					evict = &table[rnd];
+				} while(evict->present != 1);
+
+				evict->present = 0;
+				evict->present = 1;
+			}
+		}
+	}
+	return hits;
+}
+
 int main(int argc, char *argv[])
 {
 	// could be command line args
@@ -41,6 +81,27 @@ int main(int argc, char *argv[])
 		printf(", %d", sequence[i]);
 	}
 	printf("\n");
+
+	printf("# This is a benchmark of random replacement\n");
+	printf("# %d page references\n", refs);
+	printf("# %d pages \n", pages);
+	printf("#\n#\n# frames\tratio\n");
+
+	// frames is the size of the memory in frames
+	int frames;
+
+	int incr = pages/SAMPLES;
+
+	for(frames = incr; frames <= pages; frames += incr) {
+		// clear page tables entries
+		clear_page_table(table, pages);
+
+		int hits = simulate(sequence, table, refs, frames, pages);
+
+		float ratio = (float) hits/refs;
+
+		printf("%d\t%.2f\n", frames, ratio);
+	}
 
 	return 0;
 }
