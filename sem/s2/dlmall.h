7b337c1ea2c7428a02b86c2898546f9c762ebb58
Pontus Asp <pontus.asp@gmail.com>
2019-11-29 11:43:41 +0100

    s2_sanity_switch

--- sem/s2/dlmall.h
+++ sem/s2/dlmall.h
@@ -1,7 +1,11 @@
 #include <stdint.h>
 #include <sys/mman.h>
 
+#ifdef SANITY_DEBUG
 #define sanity() (insanity(__FILE__, __LINE__, __FUNCTION__));
+#else
+#define sanity() (1)
+#endif
 
 void *dalloc(size_t request);
 void dfree(void *memory);
