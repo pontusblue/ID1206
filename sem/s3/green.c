f5e2355ef4f565a0129e9cc35072aab7b6e8eee7
Pontus Asp <pontus.asp@gmail.com>
2019-12-08 14:17:23 +0100

    s3_2.3

--- sem/s3/green.c
+++ sem/s3/green.c
@@ -19,41 +19,48 @@ static green_t main_green = {
     FALSE
 };
 
-typedef struct green_rq {
-    green_t *thread;
-    green_rq *next;
-    green_rq *prev;
-};
+struct green_t *rq_head;
+struct green_t *rq_tail;
 
-struct green_rq rq_head, rq_tail;
+struct green_t *jq_head;
+struct green_t *jq_tail;
 
-void queue(green_t *thread)
+void rqueue(green_t *thread)
 {
-    struct green_rq *new = malloc(sizeof(struct green_rq));
-    new->thread = thread;
-    if(rq_head == NULL)
+    if(rq_tail != NULL)
     {
-        rq_head = new;
+        rq_tail->next = thread;
     }
-    else
-    {
-        new->next = rq_tail;
-        rq_tail->prev = new;
-    }
-    rq_tail = new;
+    rq_tail = thread;
 }
 
-green_t* pop()
+green_t* rpop()
 {
-    green_t *elem = rq_head->thread;
-    green_rq *node = rq_head;
-    rq_head = rq_head->prev;
+    green_t *tmp = rq_head;
     if(rq_head != NULL)
     {
-        rq_head->next = NULL;
+        rq_head = rq_head->next;
     }
-    free(node);
-    return elem;
+    return rq_head;
+}
+
+void jqueue(green_t *thread)
+{
+    if(jq_tail != NULL)
+    {
+        jq_tail->next = thread;
+    }
+    jq_tail = thread;
+}
+
+green_t* jpop()
+{
+    green_t *tmp = jq_head;
+    if(jq_head != NULL)
+    {
+        jq_head = jq_head->next;
+    }
+    return jq_head;
 }
 
 static green_t *running = &main_green;
@@ -89,3 +96,62 @@ int green_create(green_t *new, void *(*fun)(void*), void *arg)
     return 0;
 }
 
+void  green_thread()
+{
+    green_t *this = running;
+
+    result = (*this->fun)(this->arg);
+
+    // place waiting (joining) thread in ready queue
+    rqueue(jpop());
+
+    // save result of execution
+    this->retval = result;
+
+    // we're a zombie
+    this->zombie = TRUE;
+
+    // find the next thread to run
+    green_t *next = rpop();
+
+    running = next;
+    setcontext(next->context);
+}
+
+int green_yield()
+{
+    green_t *susp = running;
+
+    // add susp to ready queue
+    rqueue(susp);
+
+    // select the next thread for execution 
+    green_t *next = rpop();
+
+    running = next;
+    swapcontext(susp->context, next->context);
+    return 0;
+}
+
+int green_join(green_t *thread, void **res)
+{
+    if(!thread->zombie)
+    {
+        green_t *susp = running;
+
+        // add as joining thread 
+        jqueue(susp);
+
+        // select the next thread for execution
+        green_t *next = rpop();
+
+        running = next;
+        swapcontext(susp->context, next->context);
+    }
+    // collect result
+    *res = thread->retval;
+
+    // free context 
+    free(thread->context);
+    return 0;
+}
